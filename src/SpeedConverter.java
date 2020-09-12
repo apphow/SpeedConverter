d31d53ded90153c68005d4578b099d32dc2bbd86
aprilhoward <apehow111@gmail.com>
2020-09-12 15:30:27 -0400

    readability

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -5,11 +5,11 @@ public class SpeedConverter {
         if(kilometersPerHour < 0 ) {
            return  -1;
         }
-
             return Math.round(kilometersPerHour/1.609);
     }
 
     public static void printConversion(double kilometersPerHour) {
+
         if(kilometersPerHour < 0) {
             System.out.println("Invalid Value");
         } else {
